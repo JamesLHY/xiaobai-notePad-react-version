6788a079d9e06e43d57acd33e85aa785d13c10a3
James <847798512@qq.com>
2020-05-17 14:50:34 +0800

    deploy

---
+++ precache-manifest.911e30321dc5bea25d32ddceefac91cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d07b687e46e0cc1975ea1f88126bcb",
+    "url": "/xiaobai-notePad-react-version-website/index.html"
+  },
+  {
+    "revision": "0b2b270591720c13ca23",
+    "url": "/xiaobai-notePad-react-version-website/static/css/main.30276ca3.chunk.css"
+  },
+  {
+    "revision": "a0794045adf5df0e5aa4",
+    "url": "/xiaobai-notePad-react-version-website/static/js/2.5fae0eb1.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/xiaobai-notePad-react-version-website/static/js/2.5fae0eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2b270591720c13ca23",
+    "url": "/xiaobai-notePad-react-version-website/static/js/main.c7881b31.chunk.js"
+  },
+  {
+    "revision": "4ff400b007c147cb84df",
+    "url": "/xiaobai-notePad-react-version-website/static/js/runtime-main.35e75c65.js"
+  }
+]);
